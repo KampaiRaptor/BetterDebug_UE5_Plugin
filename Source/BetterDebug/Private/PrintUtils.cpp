90c8ab34fa56cbf8c7eca063db2e5cd04dba9904
Fansi129 <120315901+Fansi129@users.noreply.github.com>
2024-02-25 23:41:39 +0100

    Better print string WIP

--- Source/BetterDebug/Private/PrintUtils.cpp
+++ Source/BetterDebug/Private/PrintUtils.cpp
@@ -3,12 +3,22 @@
 
 #include "PrintUtils.h"
 
-void UPrintUtils::BetterPrintString(const FString DebugInfo, const FString Debug, const FLinearColor Color, const int Key, const float DisplayTime, const bool bShouldDisplay)
+void UPrintUtils::BetterPrintString(const FString DebugInfo, const FString Debug, const FString Key, const FPrintSetting& Settings)
 {
-	if (bShouldDisplay)
+	if (Settings.bShouldDisplay)
 	{
 		const FString DisplayString = DebugInfo + ": " + Debug;
-		const FColor DisplayColor = Color.ToFColor(true); 
-		GEngine->AddOnScreenDebugMessage(Key, DisplayTime, DisplayColor, DisplayString);
+		const FColor DisplayColor = Settings.Color.ToFColor(true);
+
+		//Setup key to 0, and change only when Key input is not "None"
+		int32 TempKey = -1;
+		if (Key != "None")
+		{
+			TempKey = FCString::Atoi(*Key);
+		}
+		
+		GEngine->AddOnScreenDebugMessage(TempKey, Settings.DisplayTime, DisplayColor, DisplayString, true, FVector2d(Settings.TextSize));
+
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *DisplayString);
 	}
 }
--- Source/BetterDebug/Public/PrintUtils.h
+++ Source/BetterDebug/Public/PrintUtils.h
@@ -9,13 +9,43 @@
 /**
  * 
  */
+
+UENUM(BlueprintType)
+enum class EDebugTypes : uint8
+{
+	Type1 UMETA(DisplayName = "Category 1"),
+	Type2 UMETA(DisplayName = "Category 2"),
+	Type3 UMETA(DisplayName = "Category 3"),
+	Type4 UMETA(DisplayName = "Category 4"),
+	Type5 UMETA(DisplayName = "Category 5"),
+};
+
+USTRUCT(BlueprintType)
+struct FPrintSetting
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PrintSetting")
+	FLinearColor Color = FLinearColor::Red;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PrintSetting")
+	float DisplayTime = 2.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PrintSetting")
+	bool bShouldDisplay = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PrintSetting")
+	float TextSize = 1.3f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PrintSetting")
+	EDebugTypes DebugType;
+};
+
 UCLASS()
 class BETTERDEBUG_API UPrintUtils : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 	
 	UFUNCTION(BlueprintCallable, Category="Debug")
-	static void BetterPrintString(const FString DebugInfo, const FString Debug, const FLinearColor Color = FLinearColor::Red, const int Key = -1, const float DisplayTime = 2.0f,const bool bShouldDisplay = true);
-	
-
+	static void BetterPrintString(const FString DebugInfo, const FString Debug, const FString Key = "None", const FPrintSetting& Settings = FPrintSetting());
 };
