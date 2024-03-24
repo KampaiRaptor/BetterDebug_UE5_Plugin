429e5d9c216d18e1b59714095d5e16d0e423a4ed
Fansi129 <120315901+Fansi129@users.noreply.github.com>
2024-03-24 22:25:54 +0100

    Print Array WIP

--- Source/BetterDebug/Private/PrintUtils.cpp
+++ Source/BetterDebug/Private/PrintUtils.cpp
@@ -1,28 +1,28 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "Misc/Variant.h"
 #include "PrintUtils.h"
 
+
 // Print String
-void UPrintUtils::BetterPrintString(const FString& DebugInfo, const FString& Debug, const FString& Key, const int DebugIndex, const FPrintSetting& Settings)
+void UPrintUtils::BetterPrintString(const FString& DebugInfo, const FString& Debug, const FString& Key, const int DebugIndex, const FPrintSetting Settings)
 {
 	if (Settings.bShouldDisplay && UPrintUtils::BoolArray[DebugIndex])
 	{
 		const FString DisplayString = DebugInfo + ": " + Debug;
-		const FColor DisplayColor = Settings.Color.ToFColor(true);
 
 		//Setup key to 0, and change only when Key input is not "None"
 		int32 const TempKey = (Key != "None") ? FCString::Atoi(*Key) : -1;
 		
 		//Print on screen
-		GEngine->AddOnScreenDebugMessage(TempKey, Settings.DisplayTime, DisplayColor, DisplayString, true, FVector2d(Settings.TextSize));
+		GEngine->AddOnScreenDebugMessage(TempKey, Settings.DisplayTime, Settings.Color.ToFColor(true), DisplayString, true, FVector2d(Settings.TextSize));
 		//Print in Log
 		UE_LOG(LogTemp, Warning, TEXT("%s"), *DisplayString);
 	}
 }
 
 // Print Float
-void UPrintUtils::PrintFloat(const FString& DebugInfo, const float& Debug, const FString& Key, const bool& bRoundFloat, const int DebugIndex, const FPrintSetting& Settings)
+void UPrintUtils::PrintFloat(const FString& DebugInfo, const float& Debug, const FString& Key, const bool& bRoundFloat, const int DebugIndex, const FPrintSetting Settings)
 {
 	if (Settings.bShouldDisplay && UPrintUtils::BoolArray[DebugIndex])
 	{
@@ -37,31 +37,28 @@ void UPrintUtils::PrintFloat(const FString& DebugInfo, const float& Debug, const
 			DisplayString = FString::Printf(TEXT("%s = %.2f"), *DebugInfo, Debug);
 		}
 		
-		const FColor DisplayColor = Settings.Color.ToFColor(true);
-
 		//Setup key to 0, and change only when Key input is not "None"
 		int32 const TempKey = (Key != "None") ? FCString::Atoi(*Key) : -1;
 		
 		//Print on screen
-		GEngine->AddOnScreenDebugMessage(TempKey, Settings.DisplayTime, DisplayColor, DisplayString, true, FVector2d(Settings.TextSize));
+		GEngine->AddOnScreenDebugMessage(TempKey, Settings.DisplayTime, Settings.Color.ToFColor(true), DisplayString, true, FVector2d(Settings.TextSize));
 		//Print in log
 		UE_LOG(LogTemp, Warning, TEXT("%s"), *DisplayString);
 	}
 }
 
-//TODO 
-void UPrintUtils::PrintStringArray(const FString& DebugInfo, const TArray<FString>& Array, const FString& Key, const int DebugIndex,const FPrintSetting& Settings)
+//TODO debug key + conditions when to print
+void UPrintUtils::PrintStringArray(const FString& DebugInfo, const TArray<FString>& Array, const FString& Key, const int DebugIndex,const FPrintSetting Settings)
 {
 	FString DisplayString = "";
 	for (int32 i = 0; i < Array.Num(); i++)
 	{
-		DisplayString += Array[i] + " \n";
+		DisplayString += DebugInfo + " " + FString::FromInt(i) + ": " + Array[i] + + " \n";
 	}
-	DisplayString = DebugInfo + ": " + DisplayString;
+	//DisplayString = DebugInfo + ": " + DisplayString;
 	DisplayString.RemoveFromEnd(" ");
 
-	GEngine->AddOnScreenDebugMessage(-1, Settings.DisplayTime, FColor::Black, DisplayString, true, FVector2d(Settings.TextSize));
+	int32 const TempKey = (Key != "None") ? FCString::Atoi(*Key) : -1;
 	
-	
+	GEngine->AddOnScreenDebugMessage(TempKey, Settings.DisplayTime, Settings.Color.ToFColor(true), DisplayString, true, FVector2d(Settings.TextSize));
 }
-
--- Source/BetterDebug/Public/PrintUtils.h
+++ Source/BetterDebug/Public/PrintUtils.h
@@ -9,6 +9,7 @@
 /**
  * 
  */
+class FVariant;
 
 USTRUCT(BlueprintType)
 struct FPrintSetting
@@ -34,15 +35,16 @@ class BETTERDEBUG_API UPrintUtils : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 	
-	UFUNCTION(BlueprintCallable, Category="Debug") //TODO can be reference
-	static void BetterPrintString(const FString& DebugInfo, const FString& Debug, const FString& Key = "None", const int DebugIndex = 0, const FPrintSetting& Settings = FPrintSetting());
+	UFUNCTION(BlueprintCallable, Category="Debug") 
+	static void BetterPrintString(const FString& DebugInfo, const FString& Debug, const FString& Key = "None", const int DebugIndex = 0, const FPrintSetting Settings = FPrintSetting());
 
 	UFUNCTION(BlueprintCallable, Category="Debug")
-	static void PrintFloat(const FString& DebugInfo, const float& Debug, const FString& Key = "None", const bool& bRoundFloat = false, const int DebugIndex = 0, const FPrintSetting& Settings = FPrintSetting());
+	static void PrintFloat(const FString& DebugInfo, const float& Debug, const FString& Key = "None", const bool& bRoundFloat = false, const int DebugIndex = 0, const FPrintSetting Settings = FPrintSetting());
 
 	UFUNCTION(BlueprintCallable, Category="Debug")
-	static void PrintStringArray(const FString& DebugInfo, const TArray<FString>& Array, const FString& Key = "None", const int DebugIndex = 0, const FPrintSetting& Settings = FPrintSetting());
-	
+	static void PrintStringArray(const FString& DebugInfo, const TArray<FString>& Array, const FString& Key = "None", const int DebugIndex = 0, const FPrintSetting Settings = FPrintSetting());
+
 public:
 	static inline TArray<bool> BoolArray{true, true, true, true, true}; 
 };
+
